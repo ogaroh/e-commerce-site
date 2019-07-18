09f83b7c162ae084c0cd5b62ee34ce89fdd7d6a0
ogaroh <ogaroerick1998@gmail.com>
2019-07-18 11:44:55 +0300

    image prep

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../../model/product';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  public product: Product;
+
   constructor() { }
 
   ngOnInit() {
+    this.product = new Product('Nike Off-White Sneakers', 'NKE' ,200);
+  }
+
+  getImageUrl () {
+    this.product.image = this.product.image;
   }
 
 }
--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,2 +1,10 @@
+// product model
 export class Product {
+
+    image: string;
+
+    constructor(
+        public name: string,
+        public code: string,
+        public price: number) { }
 }
